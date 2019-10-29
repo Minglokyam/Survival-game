38eb1b7c9c2523f2c2166a4d43dbf44710a4ef16
qianyans <2653507030@qq.com>
2019-10-29 16:12:47 -0400

    Merge branch 'master' of D:\group_0580 with conflicts.

--- phase1/SurvivalGame/app/src/main/java/com/example/survivalgame/PongGameView.java
+++ phase1/SurvivalGame/app/src/main/java/com/example/survivalgame/PongGameView.java
@@ -8,120 +8,128 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.time.Duration;
 
-public class PongGameView extends SurfaceView {
-  /** The screen width */
-  int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+public class PongGameView extends SurfaceView{
+    /** The screen width */
+    int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
 
-  /** The screen height */
-  int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+    /** The screen height */
+    int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
 
-  /** A Pong Game Thread */
-  PongGameThread thread;
+    /** A Pong Game Thread */
+    PongGameThread thread;
 
-  private boolean stop = true;
+    private boolean stop = true;
 
-  private long FPS;
+    private long FPS;
 
-  private PongGameManager pongGameManager;
+    private SurfaceHolder surfaceHolder;
 
-  private Paint paintText;
+    private Canvas canvas;
 
-  private Duration pongDuration;
+    private PongGameManager pongGameManager;
 
-  public Duration getPongDuration() {
-    return pongDuration;
-  }
+    private Paint paintText;
 
-  private PongGameActivity pongGameActivity;
+    private Duration pongDuration;
 
-  private User user;
-
-  public void setPongDuration(Duration newPongDuration) {
-    pongDuration = newPongDuration;
-  }
-
-  public PongGameView(Context context, User user) {
-    super(context);
-    pongGameActivity = (PongGameActivity) context;
-    this.user = user;
-    pongGameManager = new PongGameManager(screenWidth, screenHeight, user);
-    setFocusable(true);
-    paintText = new Paint();
-    paintText.setTextSize(36);
-    paintText.setTypeface(Typeface.DEFAULT_BOLD);
-    // =======================================
-    pongDuration = Duration.ofSeconds(9);
-  }
-
-  public void update() {
-    pongGameManager.update(FPS);
-    user.setScore(user.getScore() + 1);
-    if (user.getLife() == 0) {
-      stop = true;
-      thread.setPlaying(false);
-      pongGameActivity.toMain();
-    } else if (pongDuration.getSeconds() <= 0) {
-      stop = true;
-      thread.setPlaying(false);
-      pongGameActivity.toDodge();
+    public Duration getPongDuration(){
+        return pongDuration;
     }
-  }
 
-  public void draw(Canvas canvas) {
-    super.draw(canvas);
-    canvas.drawColor(Color.rgb(255, 255, 255));
+    private PongGameActivity pongGameActivity;
+
+    private User user;
+
+    public void setPongDuration(Duration newPongDuration){
+        pongDuration = newPongDuration;
+    }
+
+    public PongGameView(Context context, User user) {
+        super(context);
+        pongGameActivity = (PongGameActivity) context;
+        this.user = user;
+        pongGameManager = new PongGameManager(screenWidth, screenHeight, user);
+        surfaceHolder = getHolder();
+        setFocusable(true);
+        paintText = new Paint();
+        paintText.setTextSize(36);
+        paintText.setTypeface(Typeface.DEFAULT_BOLD);
+        //=======================================
+        pongDuration = Duration.ofSeconds(30);
+    }
+
+    public void update() {
+        pongGameManager.update(FPS);
+       user.setScore(user.getScore() + 1);
+        if(user.getLife() == 0){
+            stop = true;
+            thread.setPlaying(false);
+            pongGameActivity.toMain();
+        }else if(pongDuration.getSeconds() <= 0){
+            stop = true;
+            thread.setPlaying(false);
+            pongGameActivity.toDodge();
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawColor(Color.rgb(255, 255, 255));
     if (!stop) {
       canvas.drawText("Life: " + user.getLife(), 0, 32, paintText);
       canvas.drawText("Total time: " + user.getTotalDuration().getSeconds(), 0, 64, paintText);
       canvas.drawText("Game time: " + pongDuration.getSeconds(), 0, 96, paintText);
       canvas.drawText("Score: " + user.getScore(), 0, 128, paintText);
-    }
+         }
+
 
     pongGameManager.draw(canvas);
-  }
 
-  @Override
-  public boolean onTouchEvent(MotionEvent motionEvent) {
-    RectPaddle rectPaddle = pongGameManager.getRectPaddle();
-    switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-      case MotionEvent.ACTION_DOWN:
-        stop = false;
-        if (motionEvent.getX() > rectPaddle.getXCoordinate() + rectPaddle.getWidth() / 2) {
-          pongGameManager.paddleMoveRight();
-        } else {
-          pongGameManager.paddleMoveLeft();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        RectPaddle rectPaddle = pongGameManager.getRectPaddle();
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                stop = false;
+                if (motionEvent.getX() > rectPaddle.getXCoordinate() + rectPaddle.getWidth() / 2) {
+                    pongGameManager.paddleMoveRight();
+                } else {
+                    pongGameManager.paddleMoveLeft();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                pongGameManager.paddleStop();
+                break;
         }
-        break;
-      case MotionEvent.ACTION_UP:
-        pongGameManager.paddleStop();
-        break;
+        return true;
     }
-    return true;
-  }
 
-  public void pause() {
-    try {
-      thread.setPlaying(false);
-      thread.join();
-    } catch (InterruptedException e) {
-      Log.e("Error:", "joining thread");
+    public void pause() {
+        try {
+            thread.setPlaying(false);
+            thread.join();
+        } catch (InterruptedException e) {
+            Log.e("Error:", "joining thread");
+        }
     }
-  }
 
-  public void resume() {
-    thread = new PongGameThread(this, user);
-    thread.start();
-  }
+    public void resume() {
+        thread = new PongGameThread(this, user);
+        thread.start();
+    }
 
-  public void setFPS(long newFPS) {
-    FPS = newFPS;
-  }
+    public void setFPS(long newFPS) {
+        FPS = newFPS;
+    }
 
-  public boolean getStop() {
-    return stop;
-  }
+    public boolean getStop(){
+        return stop;
+    }
 }
