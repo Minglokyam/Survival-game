ba0de3add94ad271ddb20cd5e22797b696e8c691
fangmin5 <mingyuan.fang@utronoto.ca>
2019-10-29 17:00:04 -0400

    Merge branch 'master' of /Users/carryfang/Desktop/phaseddd with conflicts.

--- phase1/SurvivalGame/app/src/main/java/com/example/survivalgame/RunningGameActivity.java
+++ phase1/SurvivalGame/app/src/main/java/com/example/survivalgame/RunningGameActivity.java
@@ -7,41 +7,42 @@ import android.os.Bundle;
 
 public class RunningGameActivity extends AppCompatActivity {
 
-  RunningGameView runningGameView;
-  User user;
+    RunningGameView runningGameView;
+    User user;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_running_game);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_game);
 
-    Intent intent = getIntent();
-    user = (User) intent.getSerializableExtra("user");
-    UserUpdater.updateUser(user, User.RUNNING);
-    IOManager.saveFile();
-    runningGameView = new RunningGameView(this, user);
-    setContentView(runningGameView);
-  }
+        Intent intent = getIntent();
+        user = (User)intent.getSerializableExtra("user");
+        UserUpdater.updateUser(user, User.RUNNING);
+        IOManager.saveFile();
+        runningGameView = new RunningGameView(this, user);
+        setContentView(runningGameView);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        runningGameView.thread.running = false;
+        toPong();
+        finish();
+    }
 
-  @Override
-  protected void onPause() {
-    super.onPause();
-    runningGameView.thread.running = false;
-    finish();
-  }
+    public void toPong(){
+        Intent intent = new Intent(this, PongGameActivity.class);
+        intent.putExtra("user", user);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
 
-  public void toPong() {
-    Intent intent = new Intent(this, PongGameActivity.class);
-    intent.putExtra("user", user);
-    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    startActivity(intent);
-    finish();
-  }
-
-  public void toMain() {
+    public void toMain(){
     Intent intent = new Intent(this, MainActivity.class);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     startActivity(intent);
-    finish();
-  }
+        finish();
+    }
 }
+
