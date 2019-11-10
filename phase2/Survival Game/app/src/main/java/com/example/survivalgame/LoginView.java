877388a9377f5f4d5338c5676ef3d4a250e7ec57
Jun Da Wang <junda.wang@mail.utoronto.ca>
2019-11-10 10:58:56 -0500

    Added MVP framework

--- phase2/Survival Game/app/src/main/java/com/example/survivalgame/LoginView.java
+++ phase2/Survival Game/app/src/main/java/com/example/survivalgame/LoginView.java
@@ -1,4 +1,10 @@
 package com.example.survivalgame;
 
 public interface LoginView {
+
+    void toGame();
+    void setEmptyInputError();
+    void setUserExistError();
+    void setCredentialsError();
+
 }
