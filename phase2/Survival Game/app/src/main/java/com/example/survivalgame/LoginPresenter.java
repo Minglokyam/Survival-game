ecade42f492fcbc196934c31cc842b97a4075fec
yamming <minglok.yam@mail.utoronto.ca>
2019-11-10 12:00:25 -0500

    Use MVP.

--- phase2/Survival Game/app/src/main/java/com/example/survivalgame/LoginPresenter.java
+++ phase2/Survival Game/app/src/main/java/com/example/survivalgame/LoginPresenter.java
@@ -1,13 +1,24 @@
 package com.example.survivalgame;
 
 public class LoginPresenter implements LoginPresenterInterface {
+    private LoginView loginView;
+
+    LoginPresenter(LoginView loginView){
+        this.loginView = loginView;
+    }
 
     public void onRegisterSuccess(){}
     public void onUserNotExists(){}
     public void onCredentialEmpty(){}
-    public void launchRunningGame(String name, User user){};
-    public void launchPongGame(String name, User user){};
-    public void launchDodgeGame(String name, User user){};
+    public void launchRunningGame(String name, User user){
+        loginView.launchRunningGame(name, user);
+    }
+    public void launchPongGame(String name, User user){
+        loginView.launchPongGame(name, user);
+    }
+    public void launchDodgeGame(String name, User user){
+        loginView.launchDodgeGame(name, user);
+    }
     public void onWrongCredential(){}
 
     public LoginPresenter(LoginView loginView, LoginInteractor loginInteractor) {
