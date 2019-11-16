0c352c41a10009274eb4994b0a508fd36fdd6009
yamming <minglok.yam@mail.utoronto.ca>
2019-11-15 22:16:07 -0500

    Declare dodge game in manifest.xml

--- phase2/Survival Game/app/src/main/java/com/example/survivalgame/runninggame/RunningGameView.java
+++ phase2/Survival Game/app/src/main/java/com/example/survivalgame/runninggame/RunningGameView.java
@@ -84,7 +84,7 @@ public class RunningGameView extends SurfaceView {
 
         runningGameManager = new RunningGameManager(this);
         // =======================================
-        runningDuration = Duration.ofSeconds(3);
+        runningDuration = Duration.ofSeconds(30);
     }
 
     RunningGameThread getRunningGameThread() {
